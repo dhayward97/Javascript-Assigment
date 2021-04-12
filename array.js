b086ac4c6657704a917fe7bced0364b8553a6328
dhayward97 <71407822+dhayward97@users.noreply.github.com>
2021-04-12 19:50:33 -0400

    Add files via upload

---
+++ array.js
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+<title> Javascript Assignment</title>
+</head>
+
+<body>
+  <h1> Arrays</h1>
+<p id="demo"></p>
+<script>
+
+var pizzaToppings = ["tomato sauce", "cheese", "pepperoni"];
+console.log(pizzaToppings);
+document.getElementById("demo").innerHTML = pizzaToppings;
+
+
+</script>
+
+</body>
+
+
+</html>
